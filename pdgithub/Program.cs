ca9bbefc4d2a283e0ddace87d6cfa78f6364f844
Donát Podhorányi <donat.podhoranyi@gmail.com>
2023-12-18 12:44:35 +0100

    A `FindMinimum` függvény megkeresi és visszaadja a `numbers` listában található legkisebb számot. Ha a lista üres, akkor 0-t ad vissza, vagy egyéb értéket, ha meghatározott lenne, hogy mit jelentsen a minimum egy üres listában.

--- pdgithub/Program.cs
+++ pdgithub/Program.cs
@@ -18,6 +18,9 @@ namespace pdgithub
             Console.WriteLine("3. Feladat: Lista elemeinek kiírása");
             PrintNumbers();
 
+            Console.WriteLine("4. Feladat: Minimum kiválasztása és kiírása");
+            int minimum = FindMinimum();
+            Console.WriteLine($"A lista legkisebb eleme: {minimum}");
 
             Console.ReadKey(); // Ez itt helyezkedik el a Main metódusban
         }
@@ -45,6 +48,24 @@ namespace pdgithub
             Console.WriteLine($"Lista elemei: {string.Join(", ", numbers)}");
         }
 
-       
+        private static int FindMinimum()
+        {
+            if (numbers.Count == 0)
+            {
+                return 0; // Vagy más érték, ha nincs minimum, itt most 0-t adunk vissza
+            }
+
+            int minimum = numbers[0];
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] < minimum)
+                {
+                    minimum = numbers[i];
+                }
+            }
+
+            return minimum;
+        }
+
     }
 }
