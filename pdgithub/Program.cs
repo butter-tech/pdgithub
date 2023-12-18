55076fb2dbb1d555218a635efa523ec21f88bddf
Donát Podhorányi <donat.podhoranyi@gmail.com>
2023-12-18 12:42:31 +0100

    A `AddNumbers` függvény a felhasználótól bekéri számokat a konzolon, és minden érvényes számot hozzáad a `numbers` listához, amíg a felhasználó nem ad meg nullát. Ha a bevitt érték nem szám, akkor hibát jelez, és újra kéri a felhasználót.

--- pdgithub/Program.cs
+++ pdgithub/Program.cs
@@ -1,16 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace pdgithub
 {
     internal class Program
     {
+        private static List<int> numbers = new List<int>();
+
         static void Main(string[] args)
         {
+            Console.WriteLine("1. Feladat: Initial commit");
+            // A konzolos alkalmazás inicializálása
 
+            Console.WriteLine("2. Feladat: Számok hozzáadása (0 végjelig)");
+            AddNumbers();
+
+
+            Console.ReadKey(); // Ez itt helyezkedik el a Main metódusban
         }
+
+        private static void AddNumbers()
+        {
+            int number;
+            do
+            {
+                Console.Write("Adjon meg egy számot (0 vége): ");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out number))
+                {
+                    numbers.Add(number);
+                }
+                else
+                {
+                    Console.WriteLine("Hibás formátum! Csak számokat fogadunk el.");
+                }
+            } while (number != 0);
+        }
+
     }
 }
